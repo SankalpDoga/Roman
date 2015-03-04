e9d100fbf317a3bcd669a904039091e908b2f903
Sankalp Singh <sankalpsingh@Sankalps-MacBook-Pro.local>
2015-03-04 11:46:27 +0530

    second implementation

--- spec/roman_spec.rb
+++ spec/roman_spec.rb
@@ -4,9 +4,23 @@ describe Roman do
   [
     [ 1, 'I'],
     [ 2, 'II'],
+    [ 3, 'III'],
+    [ 4, 'IV'],
     [ 5, 'V'],
+    [ 6, 'VI'],
+    [ 7, 'VII'],
+    [ 8, 'VIII'],
     [ 9, 'IX'],
     [ 10, 'X'],
+    [ 11, 'XI'],
+    [ 12, 'XII'],
+    [ 13, 'XIII'],
+    [ 14, 'XIV'],
+    [ 15, 'XV'],
+    [ 16, 'XVI'],
+    [ 17, 'XVII'],
+    [ 18, 'XVIII'],
+    [ 19, 'XIX'],
     [ 39, 'XXXIX'],
     [ 40, 'XL'],
     [ 50, 'L'],
@@ -19,7 +33,7 @@ describe Roman do
     [ 3999, 'MMMCMXCIX'],
     [ 0, ''],
     [ 404, 'CDIV'],
-    [4000, nil]
+    [4000, '']
   ].each do |integer, roman|
     it "should convert #{integer}" do
       Roman.integer_to_roman(integer).should == roman
--- src/roman.rb
+++ src/roman.rb
@@ -1,25 +1,33 @@
 module Roman
   def self.integer_to_roman(inInteger)
-    mapping_hash = {
-      1000 => 'M',
-      500 => 'D',
-      100 => 'C',
-      50 => 'L',
-      10 => 'X',
-      5 => 'V',
-      1 => 'I'
-    }
-    keys = mapping_hash.keys
 
-    return nil if inInteger <= 0 || inInteger >= 4000
-    roman_number = ""
-    keys.each do |key|
-      (inInteger/key).times do
-        roman_number += mapping_hash[key]
-        inInteger -= key
+    roman_values = Array[ 'I', 'V', 'X', 'L', 'C', 'D', 'M']
+    ndigits = inInteger.to_s.size
+
+    i = (ndigits - 1)*2
+    roman_number = ''
+    inInteger.to_s.chars.map(&:to_i).each do |digit|
+      case digit
+      when 1..3
+        digit.times do
+          roman_number += roman_values[i]
+        end
+      when 4
+        roman_number += roman_values[i]
+        roman_number += roman_values[i+1]
+      when 5
+        roman_number += roman_values[i+1]
+      when 6..8
+        roman_number += roman_values[i+1]
+        (digit-5).times do
+          roman_number += roman_values[i]
+        end
+      when 9
+        roman_number += roman_values[i]
+        roman_number += roman_values[i+2]
       end
+      i -= 2
     end
     return roman_number
   end
 end
-
